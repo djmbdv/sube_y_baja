c1495ce8ebb0a97d93a71c8c5251d8b2c56cb779
David Marquez <djmbdv@gmail.com>
2023-12-09 14:44:48 -0400

    Graphics

--- Main.java
+++ Main.java
@@ -1,37 +1,71 @@
-import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import java.awt.Toolkit;
-import java.awt.Image;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.Console;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale.Category;
-import java.awt.image.BufferedImage;
 /**
  * InnerMain
  */
 class Carta {
     public int  number;
     public int shape;
+    public Image image;
 
-    public Carta(int number, int shape){
+    public static int ANCHO_CARTA = 201;
+    public static int ALTO_CARTA = 320;
+    public static File[] files = new File[4];
+
+    public Carta(int number, int shape) throws IOException{
         this.number = number > 7 ? number + 2 : number;
         this.shape = shape;
+
+        if(Carta.files[shape]== null){
+            Carta.files[shape] = new File(getShapeString().toLowerCase() + ".jpg");
+
+        }
+        this.image = ImageIO.read(Carta.files[shape]).getSubimage(ANCHO_CARTA * ((number-1)%5), ALTO_CARTA * ((number-1)/5),
+        ANCHO_CARTA , ALTO_CARTA);
     }
 
+    public String getShapeString(){
+    String shapeString ="";
+       switch (this.shape) {
+            case 0:
+                shapeString = "Oro";
+                break;
+            case 1:
+                shapeString = "Copas";
+                break;
+            case 2:
+                shapeString = "Espada";
+                break;
+            case 3:
+                shapeString = "Basto";
+                break;
+        
+            default:
+                break;
+        }
+
+        return shapeString;
+
+    }
     @Override
     public String toString(){
         String shapeString = "";
@@ -68,8 +102,10 @@ class Main extends JFrame {
     public JLabel cardCount;
     public JPanel panelCarta;
 
+
     public Carta getCarta(){
         System.out.println("getCarta");
+        
         int index =  ((int)(Math.random() * mazo.size()))%mazo.size();
         Carta selectedCard = mazo.get(index);
         mazo.remove(index);
@@ -77,10 +113,6 @@ class Main extends JFrame {
         return selectedCard; 
     }
 
-
-    public void setLastCarta(Carta lastCarta){
-        
-    }
     public void showLost(){
         
         JOptionPane.showMessageDialog(this.panel,"1 shut", "Bebe", JOptionPane.WARNING_MESSAGE);
@@ -125,28 +157,34 @@ class Main extends JFrame {
         this.labelLastCarta.repaint();
        this.panel.repaint();;
     }
-    public void initMazo(){
+    public void initMazo() throws IOException{
         this.mazo = new LinkedList<Carta>();
         for(int i  = 0; i < 40; i++){
             mazo.add(new Carta(i%10 + 1, i/10 ));
         }
     }
 
-    public Main(){
+    public Main() throws IOException{
         super("Carticas");
         initMazo();
-        this.panelCarta = new JPanel();
+        
+        BufferedImage i = ImageIO.read(new File("oro.jpg"));  
+        
+        this.panelCarta = new JPanel(){
+            @Override
+            public void paint(Graphics g){
+                super.paint(g);
+                Graphics2D g2 = (Graphics2D)g;
+                g2.drawImage(lastCarta.image, 0, 0, this);
+
+            }
+        };
 
         this.panelCarta.setSize(125, 300);
         this.panelCarta.setMinimumSize(new Dimension(125,300));
-        this.panelCarta.setBackground(Color.blue);
+        this.panelCarta.setBackground(Color.gray);
+      //  this.panelCarta
         this.panelCarta.setLayout(new FlowLayout());
-        
-        
-
-
-                
-     
         this.lastCarta = getCarta();
         this.cardCount = new JLabel("Restan " + String.valueOf(mazo.size()) + " cartas.");   
         this.panel = new JPanel();
@@ -189,7 +227,7 @@ class Main extends JFrame {
         repaint();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Main();
     }
 }
